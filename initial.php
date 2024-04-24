11ec607b72fb20c48c2938e847b8a6e9e8f5d483
Ron Nguyen <nguyen.ron@student.greenriver.edu>
2024-04-24 11:14:30 -0700

    update initial.php by trimming the public_html on document root for database connection.

--- initial.php
+++ initial.php
@@ -10,10 +10,12 @@
     define("PUBLIC_HTML_PATH", dirname(__FILE__));
     define("PHP_PATH", PUBLIC_HTML_PATH . "/NursingClinical/php");
     define("LAYOUTS_PATH", PHP_PATH . "/layouts");
+
     // define cookie keys
     define("SUBMITTED_SURVEY_KEY", "submitted-survey");
 
     // provide DB credentials and connect/disconnect functions
+    $_SERVER['DOCUMENT_ROOT'] = str_replace('/public_html', '', $_SERVER['DOCUMENT_ROOT']);
     require_once($_SERVER['DOCUMENT_ROOT'] . '/db-connect-nursing.php');
 
     // include all PHP helper functions
